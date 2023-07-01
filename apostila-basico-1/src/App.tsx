3efdd7c05ab4c1c74121dc3a07433518d8bb8d00
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2023-07-01 20:03:13 -0300

    aaaaaaa

---
+++ apostila-basico-1/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css'
+
+
+
+function App() {
+  return (
+    <>
+      <div id='background'>
+        <div className='tela'>
+          <header></header>
+          <main>
+            <h2 id='Lets'>Let's build the future together</h2>
+            <h3></h3>
+            <div className='sumario'>
+              <article>SUMÁRIO</article>
+              <ol>
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+                <li>d</li>
+                <li>e</li>
+              </ol>
+            </div>
+          </main>
+        </div>        
+        <footer>DESENVOLVIDO POR ABRINDO PORTAS</footer>        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ apostila-basico-1/src/capitulo1.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+function cap1() {
+
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default cap1
