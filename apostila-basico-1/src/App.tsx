1152fadea0a2f9ab377ee5354ab33ed909907ecf
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2023-07-01 22:57:32 -0300

    aaaa

--- apostila-basico-1/src/App.tsx
+++ apostila-basico-1/src/App.tsx
@@ -17,12 +17,12 @@ function App() {
               <ol>
                 <li>Verbo To-be</li>
                 <li>Preposições</li>
-                <li>c</li>
-                <li>d</li>
-                <li>e</li>
+                <li>Números</li>
               </ol>
             </div>
-            <div id='conteudo'></div>
+            <div id='conteudo'>
+              <h1 id='cap1'>CAPÍTULO 1</h1>
+              <h2>Verbo To Be</h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, tempore. Praesentium repellat doloribus delectus dolorum <span className='italico'>reprehenderit</span> sapiente et illum, similique sit accusamus <span className='negrito'>libero vel</span> iste ut adipisci explicabo, neque nesciunt?  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores numquam repudiandae aut excepturi voluptas? Non dolorem vel consequatur ducimus perspiciatis! Laudantium ut nulla suscipit quae odit, accusamus qui dolores voluptatem.</div>
           </main>
         </div>        
         <footer>DESENVOLVIDO POR ABRINDO PORTAS</footer>        
